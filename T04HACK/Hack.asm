a400076a3c9d16e29b2623d588806d1c762c3708
Vasiliy <matrenin.vn@phystech.edu>
2021-03-18 23:52:10 +0300

    made some working stuff

--- T04HACK/Hack.asm
+++ T04HACK/Hack.asm
@@ -2,38 +2,98 @@
 .code
 
 org 100h
+                               
+CONTROL_SUM 		equ	0719h
+start:           ; cleaning up all needed regs
+			xor 		ax	,	ax
+			xor		cx	,	cx
+			xor 		dx	,	dx
 
-start:
-			xor 	si	,	si
+		 ; complicated macro for bulling
+		 	
+			IRP letter, <q, w, e, r, t, y>
 
-                      	L MACRO letter
-				mov 		ah	,	01h
-				int 		21h
-				cmp 		al	, 	letter
-				JNE 		PROGRAMM
-                        ENDM
+					        ; getting symbol
+							mov 		ah	,	01h
+							int 		21h
+							mov		dl	,	al
 
-			L "P"
-			L "A"
-			L "S"
-			L "S"
-			L "W"
-			L "O"
-			L "R"
-			L "D"
-DEFAULT_READ:		L "$"
+						; bulling 
+							cmp		dx	,	'&letter&'
+							shl		dx	,	1
+							jne		bullingSkip_&letter
+									
+							jmp		Print
+			bullingSkip_&letter:
 
-			jmp	CHECK						                    
+						; counting control sum
+							and 		dx	,	2h
+							cmp 		dx	,	0
+							je		addSkip_&letter
+							add		cx	,	ax
+			addSkip_&letter:
 
-PROGRAMM:		mov 	cs:[READEN + si]	,	al
-			jmp	DEFAULT_READ
-							
+			ENDM
+
+		; control sum check                                               
+			cmp		cx	,	(CONTROL_SUM / 3)
+			mov		dx	,	offset OtherStr			
+			je		Print
+			jmp		Check		                                                                                                         
+		
+		
+Print:		; printing info                            
+			int 		21h
+
+		
+Exit:		; exit prgramm
+			mov ax, 4c00h
+			int 21h
+
+                        
+; data
+	MessageStr:		db		"  lmao, fuck u$$"
+
+	FakeStr			db		"  lmao u r gay$"
+
+	OtherStr:		db		"  biba & boba$$$"                   
 
 
+Check:		; control sum check and some bulling
+			
+			mov 		ax	, 	0900h
+             			
+			cmp		cx	,	CONTROL_SUM / 2
+			mov		dx	,	offset MessageStr
+			je 		Print
+                                                                       
+			mov		dx	,	offset FakeStr
+			mov		dx	,	offset FakeStr
+			je		Print
 
 
-READEN:			db "true-password"
+	Shirm:		db		100h dup (0)
 
-CHECK:
+			mov 		dx	,	offset MessageStr
+			cmp		cx	,	CONTROL_SUM
+			je		NoBulling
+			jmp		Print		
+
+NoBulling:		; if we don't need to bull user
+			
+			mov 		di	,	offset FakeStr
+			dec		di
+			dec 		di
+			
+			mov		si	,	[di]
+			add		si	,	'e' - '$'
+			mov   		cs:[di]	,	si
+
+			dec		di	
+			mov		si	,	[di]
+			add		si	,	'm' - 'u'
+			mov   		cs:[di]	,	si                                                                      		
+
+			jmp		Print
 
 end start
