c2bdf00636ef8ee3f590bf29bc77fe0119465557
Vasiliy Matrenin <matrenin.vn@phystech.edu>
2021-03-24 13:50:13 +0300

    added _mm stuff, not working for now

--- T05MANDEL/Mandel.cpp
+++ T05MANDEL/Mandel.cpp
@@ -32,7 +32,7 @@ void mandelDraw()
   /* Main cycle */
     while (window.isOpen ())
     {
-        float fps = (fpsCnt++) * CLOCKS_PER_SEC / (clock () - time);
+        float fps = (fpsCnt++) * CLOCKS_PER_SEC / ((float)clock () - time);
         printf ("%f" "\n", fps);
 
         /* Events processing */
@@ -47,6 +47,7 @@ void mandelDraw()
         window.clear ();
         /* Draw funcs here */
             clalcMandel (buffer);
+
             texture.loadFromImage (buffer);
             window.draw (sprite);
         /* Displaying result */
@@ -54,37 +55,77 @@ void mandelDraw()
     }
 }
 
-const size_t MAX_ITERATIONS_NUM_ = 255;
-const float  X_LIMITS_   = 1.;
-const float  Y_LIMITS_   = 1.; 
-const float  MAX_RAD_SQ_ = 1000.;
+/* Max number of counts in cycle */
+    const size_t MAX_ITERATIONS_NUM_ = 255;
+
+/* Axis limits */
+    const float  X_LIMITS_RIGH_   = +1.;
+    const float  X_LIMITS_LEFT_   = -1.;
+    const float  Y_LIMITS_TOP_   = +1.;
+    const float  Y_LIMITS_BOT_   = -1.;
+
+/* Target Radius */
+    const int32_t MAX_RAD_SQ_ = 1000;
 
 void clalcMandel( sf::Image& buff )
 {
-    for (size_t x = 0; x < WINDOW_WIDTH_; ++x)
-    {
-        float re = ((float)x / WINDOW_WIDTH_ - 0.5) * X_LIMITS_ * 2;
+    const __m128i max_rad = { (MAX_RAD_SQ_ << 32) + MAX_RAD_SQ_, (MAX_RAD_SQ_ << 32) + MAX_RAD_SQ_ };
 
-        for (size_t y = 0; y < WINDOW_HEIGHT_; ++y)
+    for (size_t y = 0; y < WINDOW_HEIGHT_; y += 4)
+    {
+            float yOneStepShift = (Y_LIMITS_TOP_  - Y_LIMITS_BOT_)  / WINDOW_HEIGHT_;
+            float im_ft_val = Y_LIMITS_BOT_  + (float) y * yOneStepShift;
+        
+            __m128 im = { im_ft_val,
+                          im_ft_val + yOneStepShift,
+                          im_ft_val + yOneStepShift * 2,
+                          im_ft_val + yOneStepShift * 3 };
+
+        for (size_t x = 0; x < WINDOW_WIDTH_; x += 4)
         {
+            float xOneStepShift = (X_LIMITS_RIGH_ - X_LIMITS_LEFT_) / WINDOW_WIDTH_;
+            float re_ft_val = X_LIMITS_LEFT_  + (float) x * xOneStepShift;
+        
+            __m128 re = { re_ft_val,
+                          re_ft_val + xOneStepShift,
+                          re_ft_val + xOneStepShift * 2,
+                          re_ft_val + xOneStepShift * 3 };
+
+            __m128i counts = {0, 0};
             size_t iters = 0;
 
-            float im = ((float)y / WINDOW_HEIGHT_ - 0.5) * Y_LIMITS_ * 2;
-
-            float transIm = im;
-            float transRe = re;
+            __m128 transIm = im;
+            __m128 transRe = re;
 
             for (; iters < MAX_ITERATIONS_NUM_; ++iters)
             {
-                if (transRe * transRe + transIm * transIm > MAX_RAD_SQ_)
+                __m128 imSq  = _mm_mul_ps (transIm, transIm);
+                __m128 reSq  = _mm_mul_ps (transRe, transRe);
+                
+                __m128  rad  = _mm_add_ps (imSq, reSq);
+
+                __m128i radi = _mm_cvttps_epi32 (rad);
+                        
+                __m128i bools = _mm_cmpeq_epi32 (radi, max_rad);
+
+                counts = _mm_add_epi32 (counts, bools);
+
+                if (_mm_test_all_zeros (radi, max_rad))
                     break;
 
-                float newIm = 2 * transIm * transRe + im;
-                transRe = transRe * transRe - transIm * transIm + re;
-                transIm = newIm;
+                transIm = _mm_add_ps (transIm, transIm);
+                transIm = _mm_mul_ps (transIm, transRe);
+                transIm = _mm_add_ps (transIm, im);
+
+                transRe = _mm_sub_ps (reSq, imSq);
+                transRe = _mm_add_ps (reSq, re);
             }
 
-            buff.setPixel (x, y, getColor (iters));
+            buff.setPixel (x    , y    , getColor (counts[0] & 0xFFFFFFFF));
+            buff.setPixel (x + 1, y + 1, getColor (counts[0] >> 32));
+            buff.setPixel (x + 2, y + 2, getColor (counts[1] & 0xFFFFFFFF));
+            buff.setPixel (x + 3, y + 3, getColor (counts[1] >> 32));
+            
         }
     }
 }
--- T05MANDEL/Mandel.h
+++ T05MANDEL/Mandel.h
@@ -7,6 +7,11 @@
 #include <time.h>
 #include <math.h>
 
+#include <pmmintrin.h>
+#include <xmmintrin.h>
+#include <emmintrin.h>
+#include <smmintrin.h>
+
 #include <SFML/Graphics.hpp>
 
 void mandelDraw();
